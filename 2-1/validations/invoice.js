327af8369977689fb5a48d416129e140826b986f
mohammadgk1993 <mohammadgk1993@gmail.com>
2023-04-23 23:03:57 +0330

    6th commit

--- 2-1/validations/invoice.js
+++
@@ -1,7 +0,0 @@
-const Joi = require("joi");
-
-const invoiceValidationSchema = Joi.object({
-  transactionCode: Joi.string().min(12).max(12).required(),
-});
-
-module.exports = { invoiceValidationSchema };
